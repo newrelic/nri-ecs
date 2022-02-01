de790798344f46575fc4f0d66b311bb46b5dd9af
Alvaro Cabanas <acabanas@newrelic.com>
2022-02-01 13:17:51 +0100

    Remove already tested behaviour

--- internal/infra/entity_test.go
+++ internal/infra/entity_test.go
@@ -98,42 +98,7 @@ func Test_PopulateIntegrationFargate(t *testing.T) {
 		require.Len(t, i.Entities, 2)
 		clusterEntity := i.Entities[0]
 
-		t.Run("generates_the_cluster_entity", func(t *testing.T) {
-			t.Parallel()
-
-			assert.Equal(t, "cluster/ecs-local-cluster", i.Entities[0].Metadata.Name)
-			assert.Equal(t, "arn:aws:ecs:us-west-2:xxxxxxxx", i.Entities[0].Metadata.Namespace)
-		})
-
-		t.Run("generates_inventory", func(t *testing.T) {
-			t.Parallel()
-
-			item, ok := clusterEntity.Inventory.Item("cluster")
-
-			assert.True(t, ok, "inventory not found")
-			assert.Equal(t, clusterMetadata.Name, item["name"])
-			assert.Equal(t, clusterMetadata.ARN, item["arn"])
-		})
-
-		t.Run("add_cluster_metadata_to_local_entity", func(t *testing.T) {
-			t.Parallel()
-
-			e := i.LocalEntity()
-
-			ecsCluster, ok := e.Inventory.Item("host")
-			assert.True(t, ok, "inventory not found")
-
-			expected := inventory.Item(map[string]interface{}{
-				"ecsClusterName": clusterMetadata.Name,
-				"ecsClusterArn":  clusterMetadata.ARN,
-				"awsRegion":      clusterMetadata.Region,
-				"ecsLaunchType":  clusterMetadata.LaunchType,
-			})
-
-			assert.Equal(t, expected, ecsCluster)
-		})
-
-		t.Run("add_heartbeat_metric_set", func(t *testing.T) {
+		t.Run("add_heartbeat_metric_set_has_all_metrics", func(t *testing.T) {
 			t.Parallel()
 
 			assert.Len(t, clusterEntity.Metrics, 1)
